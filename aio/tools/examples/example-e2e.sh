67161f84988a0d78d6bd2c29c9bca3d70b4fc443
Brandon <robertsbt@gmail.com>
2019-02-28 19:18:08 +0000

    docs: more build stuff

--- aio/tools/examples/example-e2e.sh
+++ aio/tools/examples/example-e2e.sh
@@ -4,12 +4,7 @@ set -eu -o pipefail
 
 echo $(pwd)
 (
-  # yarn --cwd=aio/tools/examples/shared --frozen-lockfile
-  cd aio/content/examples/toh-pt6
-  # cp -R ../../../tools/examples/shared/node_modules .
-  cp -L -R ../../../tools/examples/shared/boilerplate/cli/* .
-
-  # Build and store the app
-  yarn
-  ng e2e --prod --no-webdriver-update
+  cd aio
+  yarn example-setup
+  yarn example-e2e --filter router
 )
