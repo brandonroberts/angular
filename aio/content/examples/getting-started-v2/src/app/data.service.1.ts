7261e232812a3f4767cbae49fa0597e9cef720dd
Brandon <robertsbt@gmail.com>
2019-03-07 15:56:04 +0000

    docs: create separate doc for v2 example changes

---
+++ aio/content/examples/getting-started-v2/src/app/data.service.1.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getCartItems() {
+    const cartItems = this.items.reduce((items, product) => {
+      if (!items[product.id]) {
+        items[product.id] = this.items.filter(item => item.id === product.id).length;
+      }
+
+      return items;
+    }, {});
+
+    const ids = Object.keys(cartItems);
+
+    return ids.map(id => {
+      const product = this.items.find(prod => prod.id === +id);
+      const quantity = cartItems[id];
+
+      return { product, quantity };
+    });  
+  }
+}
